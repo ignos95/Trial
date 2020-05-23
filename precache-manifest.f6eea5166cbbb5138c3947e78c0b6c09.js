350123119751a9d73633e4080997a85ee70a66f7
ignos95 <ignos.c.a@gmail.com>
2020-05-23 12:28:04 +0530

    Updates

---
+++ precache-manifest.f6eea5166cbbb5138c3947e78c0b6c09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d091c872f8ea6beb6da67cae0d94462",
+    "url": "/Trial/index.html"
+  },
+  {
+    "revision": "64dcd82d575971aaeec4",
+    "url": "/Trial/static/css/main.943a4a08.chunk.css"
+  },
+  {
+    "revision": "e84dd9cdf94ec49a7d57",
+    "url": "/Trial/static/js/2.13f05cf0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Trial/static/js/2.13f05cf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64dcd82d575971aaeec4",
+    "url": "/Trial/static/js/main.43966414.chunk.js"
+  },
+  {
+    "revision": "f16241713959c37e3d09",
+    "url": "/Trial/static/js/runtime-main.9c6cf337.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Trial/static/media/burger-logo.b8503d26.png"
+  }
+]);
